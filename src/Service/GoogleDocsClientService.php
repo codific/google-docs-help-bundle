e00a5fb0acced0e0c21bbe80e2f6723992b2bf3e
Zlatomir Haralambov <zlatomir@codific.eu>
2022-06-28 16:37:54 +0300

    Updated redis cache expiration to not expire

--- src/Service/GoogleDocsClientService.php
+++ src/Service/GoogleDocsClientService.php
@@ -506,7 +506,7 @@ class GoogleDocsClientService
     {
         return $this->redisCache->get($this->redisTag, function (ItemInterface $item) {
             $item->tag($this->redisTag);
-            $item->expiresAfter(3600); // 1 hour
+            $item->expiresAfter(null); // stored permanently or for as long as the implementation allows
             if (!$this->enabled) {
                 return [[], []];
             }
